114e3414d6ebb9d0aa84a5a89a34be8b651da210
David Steinich <drsteini@usgs.gov>
2015-03-31 07:43:48 -0500

    Need to include new objects in the commit...

---
+++ src/test/java/gov/usgs/cida/qw/CustomStringToArrayConverterTest.java
@@ -0,0 +1,24 @@
+package gov.usgs.cida.qw;
+
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.Test;
+
+public class CustomStringToArrayConverterTest {
+
+	CustomStringToArrayConverter conv = new CustomStringToArrayConverter();
+	
+	@Test
+	public void convertTest() {
+		//NPE
+		assertArrayEquals(new String[0],conv.convert(null));
+		
+		//Happy paths
+		assertArrayEquals(new String[0], conv.convert(""));
+		
+		assertArrayEquals(new String[]{"abc"}, conv.convert("abc"));
+
+		assertArrayEquals(new String[]{"a,b,c"}, conv.convert("a,b,c"));
+
+		assertArrayEquals(new String[]{"a","b,c"}, conv.convert("a;b,c"));
+	}
+}
