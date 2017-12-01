a4a96a1185f440c4663064f1303bea58366e0be9
Mary Bucknell <mbucknell@usgs.gov>
2017-12-01 11:13:22 -0600

    Add additional tests and remove unnecessary pom dependencies

--- src/test/java/gov/usgs/cida/qw/codes/webservices/MonitoringLocationRestControllerTest.java
+++ src/test/java/gov/usgs/cida/qw/codes/webservices/MonitoringLocationRestControllerTest.java
@@ -19,20 +19,36 @@ public class MonitoringLocationRestControllerTest extends BaseCodesRestControlle
 	public static String CODE_VALUE = "USGS-07083000";
 	public static String CODE_JSON = "{\"value\":\"USGS-07083000\",\"desc\":\"HALFMOON CREEK NEAR MALTA, CO\",\"providers\":\"BIODATA NWIS\"}";
 	public static String CODE_XML = XML_HEADER + "<Code value=\"USGS-07083000\" desc=\"HALFMOON CREEK NEAR MALTA, CO\" providers=\"BIODATA NWIS\"/>";
+	
 	public static String SEARCH_TEXT = "USGS-070837";
 	public static String SEARCH_JSON = "{\"codes\":[{\"value\":\"USGS-07083710\",\"desc\":\"ARKANSAS RIVER BELOW EMPIRE GULCH NEAR MALTA, CO\",\"providers\":\"NWIS\"}],\"recordCount\":2}";
-	public static String SEARCH_XML = XML_HEADER + "<Codes><Code value=\"USGS-07083710\" desc=\"ARKANSAS RIVER BELOW EMPIRE GULCH NEAR MALTA, CO\" providers=\"NWIS\"/><recordCount>2</recordCount></Codes>"; 
+	public static String SEARCH_XML = XML_HEADER + "<Codes><Code value=\"USGS-07083710\" desc=\"ARKANSAS RIVER BELOW EMPIRE GULCH NEAR MALTA, CO\" providers=\"NWIS\"/><recordCount>2</recordCount></Codes>"; 	
+	
+	public static String SEARCH_TEXT_WITH_ORGID="USGS&organizationid=USGS-CO";
+	public static String SEARCH_JSON_WITH_ORGID = "{\"codes\":[{\"value\":\"USGS-07083200\",\"desc\":\"HALFMOON CR BL HALFMOON DIVERSION NR LEADVILLE, CO\",\"providers\":\"NWIS\"}],\"recordCount\":3}";
+	public static String SEARCH_XML_WITH_ORGID = XML_HEADER + "<Codes><Code value=\"USGS-07083200\" desc=\"HALFMOON CR BL HALFMOON DIVERSION NR LEADVILLE, CO\" providers=\"NWIS\"/><recordCount>3</recordCount></Codes>"; 	
+	
+	public static String SEARCH_TEXT_WITH_PROVIDER="USGS&provider=NWIS";
+	public static String SEARCH_JSON_WITH_PROVIDER = "{\"codes\":[{\"value\":\"USGS-07083200\",\"desc\":\"HALFMOON CR BL HALFMOON DIVERSION NR LEADVILLE, CO\",\"providers\":\"NWIS\"}],\"recordCount\":5}";
+	public static String SEARCH_XML_WITH_PROVIDER= XML_HEADER + "<Codes><Code value=\"USGS-07083200\" desc=\"HALFMOON CR BL HALFMOON DIVERSION NR LEADVILLE, CO\" providers=\"NWIS\"/><recordCount>5</recordCount></Codes>"; 	
+	
 	public static String COMPARE_FILE_JSON = "monitoringLocation.json";
 	public static String COMPARE_FILE_XML = "monitoringLocation.xml";
+
 
 	@Test
 	public void getListAsJsonTest() throws Exception {
 		runGetListAsJsonTest(TEST_ENDPOINT, SEARCH_TEXT, COMPARE_FILE_JSON, SEARCH_JSON);
+		runGetListAsJsonTest(TEST_ENDPOINT, SEARCH_TEXT_WITH_ORGID, COMPARE_FILE_JSON, SEARCH_JSON_WITH_ORGID);
+		runGetListAsJsonTest(TEST_ENDPOINT, SEARCH_TEXT_WITH_PROVIDER, COMPARE_FILE_JSON, SEARCH_JSON_WITH_PROVIDER);
+
 	}
 
 	@Test
 	public void getListAsXmlTest() throws Exception {
 		runGetListAsXmlTest(TEST_ENDPOINT, SEARCH_TEXT, COMPARE_FILE_XML, SEARCH_XML);
+		runGetListAsXmlTest(TEST_ENDPOINT, SEARCH_TEXT_WITH_ORGID, COMPARE_FILE_XML, SEARCH_XML_WITH_ORGID);
+		runGetListAsXmlTest(TEST_ENDPOINT, SEARCH_TEXT_WITH_PROVIDER, COMPARE_FILE_XML, SEARCH_XML_WITH_PROVIDER);
 	}
 
 	@Test
