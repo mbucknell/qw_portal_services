abacf00c4160dcc20f556f443c4b89a46e446b4b
Mary Bucknell <mbucknell@usgs.gov>
2018-07-03 09:48:07 -0500

    Added corsConfiguration

--- src/main/java/gov/usgs/cida/qw/springinit/OAuth2ResourceServerConfig.java
+++ src/main/java/gov/usgs/cida/qw/springinit/OAuth2ResourceServerConfig.java
@@ -1,7 +1,9 @@
 package gov.usgs.cida.qw.springinit;
 
+import com.google.common.collect.ImmutableList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
@@ -12,6 +14,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableResourceServer
@@ -33,7 +38,6 @@ public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter 
 			.authorizeRequests()
 				.antMatchers("/", "/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/v2/**").permitAll()
 				.antMatchers("/version/**", "/actuator/health", "/actuator/info").permitAll()
-				.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 				.anyRequest().fullyAuthenticated()
 			.and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -51,4 +55,21 @@ public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter 
 			.tokenServices(tokenServices)
 			.tokenStore(tokenStore);
 	}
+	
+	@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(ImmutableList.of("*"));
+        configuration.setAllowedMethods(ImmutableList.of("HEAD", "GET", "POST"));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+	
 }
