4412dbb6c90b6e83623ef2e4c81978afc60e1316
Mary Bucknell <mbucknell@usgs.gov>
2018-12-14 11:16:42 -0600

    Fixed test and reverted CORs changes

--- src/main/java/gov/usgs/cida/qw/springinit/OAuth2ResourceServerConfig.java
+++ src/main/java/gov/usgs/cida/qw/springinit/OAuth2ResourceServerConfig.java
@@ -1,14 +1,21 @@
 package gov.usgs.cida.qw.springinit;
 
+import com.google.common.collect.ImmutableList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableResourceServer
@@ -25,11 +32,43 @@ public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter 
 	private TokenStore tokenStore;
 
 	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/", "/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/v2/**").permitAll()
+				.antMatchers("/version/**", "/actuator/health", "/actuator/info").permitAll()
+				.anyRequest().fullyAuthenticated()
+			.and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+				.cors()
+			.and()
+				.csrf().disable()
+		;
+	}
+
+	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) {
 		//This is required to enable limiting by resourceId.
 		resources.resourceId(resourceId)
 			.tokenServices(tokenServices)
 			.tokenStore(tokenStore);
 	}
+	
+	@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(ImmutableList.of("*"));
+        configuration.setAllowedMethods(ImmutableList.of("HEAD", "GET", "POST"));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 	
 }
--- src/test/java/gov/usgs/cida/qw/VersionControllerTest.java
+++ src/test/java/gov/usgs/cida/qw/VersionControllerTest.java
@@ -10,15 +10,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import gov.usgs.cida.qw.springinit.DBTestConfig;
 import gov.usgs.cida.qw.springinit.SpringConfig;
 
 @RunWith(SpringRunner.class)
+@ActiveProfiles("it")
 @AutoConfigureMockMvc(secure=false)
 @SpringBootTest(webEnvironment=WebEnvironment.MOCK,
-	classes={SpringConfig.class, CustomStringToArrayConverter.class,
+	classes={DBTestConfig.class, SpringConfig.class, CustomStringToArrayConverter.class,
 			VersionController.class})
 public class VersionControllerTest {
 
