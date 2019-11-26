0aae0dd4367498c5cb5c507d79eee3b7dd2efa77
David Steinich <drsteini@contractor.usgs.gov>
2019-11-26 15:53:07 -0600

    redirect 302 & swagger server

--- src/main/java/gov/usgs/cida/qw/springinit/OpenApiConfig.java
+++ src/main/java/gov/usgs/cida/qw/springinit/OpenApiConfig.java
@@ -1,20 +1,26 @@
 package gov.usgs.cida.qw.springinit;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class OpenApiConfig {
 
 	public static final String LOOKUP_TAG_DESCRIPTION = "Lookup and Validate";
 
+	@Value("${SWAGGER_SERVER_URL}")
+	private String serverUrl;
+
 	@Bean
 	public OpenAPI customOpenAPI() {
 		return new OpenAPI()
+				.addServersItem(new Server().url(serverUrl))
 				.components(new Components())
 				.info(new Info().title("Water Quality Portal Codes API").description(
 						"Documentation for the Water Quality Portal Codes Lookup and Validation API"));
